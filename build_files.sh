1e7bb21bf4cfeeb180558ba3264155b2bdf3f53c
Gideon Amienghemhen <amienz0958@gmail.com>
2025-06-28 03:09:58 +0100

    Made adjustments to the requirement.txt file

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies for PostgreSQL
+apt-get update -qq && apt-get install -y postgresql-client
+
+# Install Python dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate --noinput 
